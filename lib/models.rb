ada72c35236a1a8310c7ccd5b09b1161a0ec3f0c
Edwin Pratomo <edwin@insep.si>
2018-02-18 09:09:17 +0700

    factored out AR models

---
+++ lib/models.rb
@@ -0,0 +1,46 @@
+require 'active_record'
+require 'yaml'
+require 'pp'
+require 'match_state'
+require 'diff_reader'
+
+class Match < ActiveRecord::Base
+  has_many :snapshots
+end
+
+class Snapshot < ActiveRecord::Base
+  belongs_to :match
+
+  after_initialize :init_state
+  before_save :generate_diff
+  after_save DiffReader.new
+
+  def previous
+    @previous ||= Snapshot.where(match: match).where("created_at < NOW()").order(:created_at).last
+  end
+
+  def parsed
+    @parsed ||= JSON.parse(raw)
+  end
+
+  private
+  def init_state
+    state_classes = {
+      "registration" => MatchRegistration, 
+      "in_progress" => MatchInProgress,
+      "finished" => MatchFinished
+    }
+    @state = state_classes[parsed["status"]].new(my_team)
+  end
+
+  def my_team
+    %w[team1 team2].find {|e| parsed["teams"][e]["@id"].split('/').last == ::MY_CLUB }
+  end
+
+  def generate_diff
+    return true unless previous
+    hsh = @state.run_checks(previous, self)
+    throw(:abort) if hsh.empty?
+    self.diff = JSON.pretty_generate(hsh)
+  end
+end
