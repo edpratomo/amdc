c9009963fa13901f89c1304c08456b23efe25cbf
Edwin Pratomo <edwin@insep.si>
2018-02-21 01:10:11 +0700

    added monitor-all.sh

---
+++ bin/monitor-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONFIG_FILE=config/matches_list.txt
+
+if [ -f "$CONFIG_FILE" ];then
+  IFS=$'\n'; for line in `cat "$CONFIG_FILE"`; do
+    match_id=$(echo "$line" | awk '{print $1}')
+    desc=$(echo "$line" | cut -d ' ' -f 2-)
+
+    echo "$desc"
+    if [ "$RAILS_ENV" == "production" ];then
+      . ./bin/monitor-team-match.sh $match_id | ./bin/telegram-bot.sh
+    else
+      . ./bin/monitor-team-match.sh $match_id 
+    fi
+  done
+fi
