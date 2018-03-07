34a10a6c7a7bed307d1fb4ded0ed925fdc4b2e76
Edwin Pratomo <edwin@insep.si>
2018-03-07 09:29:25 +0700

    schedule another check close to start time

--- check-min-players.rb
+++ check-min-players.rb
@@ -21,4 +21,12 @@ if parsed["teams"][my_team]["players"].size < parsed["settings"]["min_team_playe
   message = "Bros, kita masih kurang #{delta} pemain lagi untuk match melawan #{url_snippet}, " +
             "start tanggal #{parsed["start_time"].human_datetime.escape_telegram_markdown}"
   print message
+
+  # schedule another check
+  check_time = parsed["start_time"].to_i - 12 * 3600
+  if Time.now.to_i < check_time
+    $stderr.puts "[INFO] Scheduled for checking min players at #{check_time.human_datetime} in match against #{parsed["teams"][opponent]["name"]}"
+    source_id = parsed['@id'].split('/').last
+    ClockWorker.perform_at(check_time, source_id)
+  end
 end
